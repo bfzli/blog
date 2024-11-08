6240adc4c6e58c8b39fef8a64920ac90072d6843
Benjamin <bfzli@hotmail.com>
2024-11-08 04:58:21 +0100

    @bfzli | some fixes

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -8,6 +8,8 @@ const profile = {
     email: 'mailto:bfzli@hotmail.com',
     github: 'https://github.com/bfzli',
     twitter: 'https://twitter.com/bfzli',
+    resume: 'https://bfzli.com/me.pdf',
+    linkedIn: 'https://www.linkedin.com/in/benjaminfzl'
   }
 }
 
@@ -17,6 +19,8 @@ export const constants = {
   links: {
     external: [
       { name: 'Email', link: profile.links.email },
+      { name: 'Resume', link: profile.links.resume },
+      { name: 'LinkedIn', link: profile.links.linkedIn },
       { name: 'GitHub', link: profile.links.github },
       { name: 'X', link: profile.links.twitter }
     ]
